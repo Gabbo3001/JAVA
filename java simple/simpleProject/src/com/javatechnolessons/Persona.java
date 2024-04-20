ed8631ceb9c412a923faecea58168ce85c3acdf8
JGabriel Bustinza <114235606+Gabbo3001@users.noreply.github.com>
2024-04-20 10:30:55 -0500

    Delete java simple/simpleProject/src/com/javatechnolessons directory

--- java simple/simpleProject/src/com/javatechnolessons/Persona.java
+++
@@ -1,5 +0,0 @@
-package com.javatechnolessons;
-
-public class Persona {
-
-}
--- java simple/simpleProject/src/com/javatechnolessons/services/IPersonaService.java
+++
@@ -1,7 +0,0 @@
-package com.javatechnolessons.services;
-
-import com.javatechnolessons.model.Persona;
-
-public interface IPersonaService {
-    public void registrar(Persona persona);
-}
